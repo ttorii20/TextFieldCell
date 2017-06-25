d72d31d4ce9598f8665ebed2459daed61348c1cf
鳥居隆弘 <toriitakahiro@toriitakahiro-no-MacBook-Air.local>
2017-06-25 17:56:34 +0900

    first commit

---
+++ TextFieldInCustomCell/TextFieldCell.swift
@@ -0,0 +1,24 @@
+//
+//  TextFieldCell.swift
+//  TextFieldInCustomCell
+//
+//  Created by 鳥居隆弘 on 2017/06/25.
+//  Copyright © 2017年 鳥居隆弘. All rights reserved.
+//
+
+import UIKit
+
+class TextFieldCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
